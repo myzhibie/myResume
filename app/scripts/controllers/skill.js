c1936685a7e272414d72a6799de9bb8b3da5a6cd
myzhibie <236752395@qq.com>
2015-03-08 14:52:30 +0800

    update for myskill

--- app/scripts/controllers/skill.js
+++ app/scripts/controllers/skill.js
@@ -9,3 +9,25 @@ angular.module('myproApp')
             templateUrl:'../../views/skill.html'
         }
     })
+    .directive('myZoom',function(){
+        return function(scope,element,attr){
+
+            element.on('mouseover',function(){
+                myAnimate(element[0],{
+                    'width':300,
+                    'height':300,
+                    'left':-25,
+                    'top':-25
+                },500);
+            }).on('mouseout',function(){
+                myAnimate(element[0],{
+                    'width':250,
+                    'height':250,
+                    'left':0,
+                    'top':0
+                },500);
+            })
+
+
+        }
+    });
